d9af849cc14c174d3c6f39f1d35e0865e56c8911
mikasa <1244456115@qq.com>
2025-06-29 02:43:35 +0800

    Compatible with Carpet #4

--- src/main/java/me/fallenbreath/tweakermore/mixins/tweaks/mc_tweaks/flyDrag/LivingEntityMixin.java
+++ src/main/java/me/fallenbreath/tweakermore/mixins/tweaks/mc_tweaks/flyDrag/LivingEntityMixin.java
@@ -28,8 +28,8 @@ import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin extends Entity {
@@ -37,13 +37,25 @@ public abstract class LivingEntityMixin extends Entity {
         super(type, world);
     }
 
-    @ModifyConstant(
+    @ModifyVariable(
             //#if MC >= 12103
             //$$ method = "travelInAir",
             //#else
             method = "travel",
             //#endif
-            constant = @Constant(floatValue = 0.91F)
+            at = @At(
+                    value = "INVOKE",
+                    //#if MC >= 11700
+                    target = "Lnet/minecraft/world/entity/LivingEntity;handleRelativeFrictionAndCalculateMovement(Lnet/minecraft/world/phys/Vec3;F)Lnet/minecraft/world/phys/Vec3;"
+                    //#elseif MC >= 11600
+                    //$$ target = "Lnet/minecraft/entity/LivingEntity;method_26318(Lnet/minecraft/util/math/Vec3d;F)Lnet/minecraft/util/math/Vec3d;"
+                    //#elseif MC >= 11500
+                    //$$ target = "Lnet/minecraft/entity/LivingEntity;applyClimbingSpeed(Lnet/minecraft/util/math/Vec3d;)Lnet/minecraft/util/math/Vec3d;"
+                    //#else
+                    //$$ target = "Lnet/minecraft/entity/LivingEntity;method_18801(Lnet/minecraft/util/math/Vec3d;)Lnet/minecraft/util/math/Vec3d;"
+                    //#endif
+            ),
+            ordinal = 1
     )
     private float creativeFlyDrag(float dragFactor) {
         LivingEntity self = (LivingEntity) (Object) this;
